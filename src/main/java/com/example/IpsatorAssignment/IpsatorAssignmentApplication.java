a927a435217bb0e0b3aaf61a379b38d611a56e86
yshubham1898 <yshubham1898@gmail.com>
2022-06-25 19:41:56 +0530

    initial commit

---
+++ src/main/java/com/example/IpsatorAssignment/IpsatorAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.IpsatorAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpsatorAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpsatorAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IpsatorAssignment/IpsatorAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IpsatorAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpsatorAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
